ca0320ae7ace086e3ffa7318b5ec1ca379636c60
FelipeCapilla <fcapilla@udesa.edu.ar>
2023-11-14 11:19:29 -0300

    g

---
+++ js/detailserie.js
@@ -0,0 +1,34 @@
+let apikey = "c5954851f3946cd861a8968314472164"
+let contenedordetail = document.querySelector(".detailserie")
+let queryString = location.search
+let queryStringObj = new URLSearchParams(queryString)
+
+fetch(`https://api.themoviedb.org/3/tv?api_key=${apikey}`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+        for (let i = 0; i < data.results.length; i++){
+            contenedordetail.innerHTML += `
+            <article>
+            <img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}" height="300px" width="200px" />
+            <h1>
+            ${data.results[i].name}
+            </h1>
+            <h2>
+            ${data.results[i].average}
+            </h2>
+            <h3>
+            ${data.results[i].date}
+            </h3>
+            <h4>
+            ${data.results[i].overview}
+            </h4>
+            </article>
+            `
+        }
+    })
+    .catch(function(error){
+        console.log('El error es:' + error);
+    })
