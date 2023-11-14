d3f8cbd2245a6d969f0c2603c3cebea336764aa4
FelipeCapilla <fcapilla@udesa.edu.ar>
2023-11-14 11:09:51 -0300

    g

--- js/detailmovie.js
+++ js/detailmovie.js
@@ -2,7 +2,8 @@ let apikey = "c5954851f3946cd861a8968314472164"
 let contenedordetail = document.querySelector(".detailpelicula")
 let queryString = location.search
 let queryStringObj = new URLSearchParams(queryString)
-fetch(`https://api.themoviedb.org/3/movie/{queryStringObj.get('buscador')}`)
+
+fetch(`https://api.themoviedb.org/3/movie?api_key=${apikey}`)
     .then(function(response){
         return response.json();
     })
