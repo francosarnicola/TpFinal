c3c62799de684151f8abf488915adb7dbb36c2d4
FelipeCapilla <fcapilla@udesa.edu.ar>
2023-11-16 15:52:55 -0300

    l

---
+++ js/searchresults.js
@@ -0,0 +1,10 @@
+fetch()
+    .then(function(response){
+        return response.json
+    })
+    .then(function(data){
+        console.log(data)
+    })
+    .catch(function(error){
+        console.log("El error es: " + error)
+    })
