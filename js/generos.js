4088666c4e105d33e1c65578685d0725dc9d2796
Franco Sarnicola <francosarnicola@MBP-de-Franco-3.fibertel.com.ar>
2023-11-16 16:22:39 -0300

    cambios

--- js/generos.js
+++ js/generos.js
@@ -2,7 +2,7 @@ let apikey = "c5954851f3946cd861a8968314472164"
 let generospeliculas = document.querySelector(".contenedorpeliculas")
 fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apikey}`)
     .then(function(response){
-        return response.json
+        return response.json();
     })
     .then(function(data){
         console.log(data)
