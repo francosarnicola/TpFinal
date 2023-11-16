0d92e2a9efeec9fe314df7a0beb51d3280d3da1c
FelipeCapilla <fcapilla@udesa.edu.ar>
2023-11-16 16:18:54 -0300

    l

--- js/generos.js
+++ js/generos.js
@@ -1,9 +1,16 @@
-fetch()
+let apikey = "c5954851f3946cd861a8968314472164"
+let generospeliculas = document.querySelector(".contenedorpeliculas")
+fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apikey}`)
     .then(function(response){
         return response.json
     })
     .then(function(data){
         console.log(data)
+        for (let i = 0; i < data.results.length; i++){
+            generospeliculas.innerHTML += `
+            
+            `
+        }
     })
     .catch(function(error){
         console.log("El error es: " + error)
